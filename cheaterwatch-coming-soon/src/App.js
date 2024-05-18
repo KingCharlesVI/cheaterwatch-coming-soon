494d992482e2ce1069c7efd32fed847f138ab4d8
KingCharlesVI <89871965+KingCharlesVI@users.noreply.github.com>
2024-05-18 12:25:30 +0100

    First commit

---
+++ cheaterwatch-coming-soon/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Coming Soon</h1>
+        <p>We are working hard to build this website. Check back soon for more info.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
