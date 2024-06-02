9336126a8fd5d70a9c4e71bc5d02551e69d53e97
KingCharlesVI <89871965+KingCharlesVI@users.noreply.github.com>
2024-06-02 18:08:31 +0100

    Update MainContent.js

--- cheaterwatch-coming-soon/src/components/MainContent.js
+++ cheaterwatch-coming-soon/src/components/MainContent.js
@@ -7,7 +7,7 @@ const MainContent = () => {
             <h2>Coming Soon!</h2>
             <p>We are currently working hard on this project. Stay tuned for updates.</p>
             <p>Join our <a href="https://discord.com/invite/your-discord-invite" target="_blank" rel="noopener noreferrer">Discord</a> server to learn more and to find out how you can get involved.</p>
-            <p>There will be a beta test before the full release.</p>
+            <p>CLOSED BETA WILL RELEASE ON JUNE 20TH @1PM BST</p>
             <p>-The CheaterWatch team</p>
         </div>
     );
